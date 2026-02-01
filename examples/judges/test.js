7b4c7228f3727850f27f00677fb2267ee855e0aa
angelator312 <112373005+angelator312@users.noreply.github.com>
2026-02-01 12:55:56 +0200

    Update test.js

--- examples/judges/test.js
+++ examples/judges/test.js
@@ -3,7 +3,7 @@ SetCpp(...files);
 SetTimeout(5000);
 
 for (const testFile of files) {
-  const what = SubmitCode(testFile, { openBrowser: false });
-  Log(testFile, ":", what ? 0 : 1);
+  const what = await SubmitCode(testFile, { openBrowser: false });
+  Log(testFile, ":", what);
   if (!what) Fail("Failing");
 }
