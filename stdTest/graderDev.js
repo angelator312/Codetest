84911758b03b1e7d0dc43bce485d25e0486c6e13
Angel Raev <112373005+angelator312@users.noreply.github.com>
2026-03-28 05:37:16 +0200

    added .h fiile in watchables(so it can be pushed)

--- stdTest/graderDev.js
+++ stdTest/graderDev.js
@@ -4,6 +4,7 @@ if (typeof CPP !== "string") throw new Error("CPP isn't valid");
 SetConfig({ watch: true });
 SetWatchables(CPP, CPP.replace(".cpp", ".d/*.in"));
 SetWatchables(CPP, CPP.replace(".cpp", ".d/*.sol"));
+SetWatchables(CPP, CPP.replace(".cpp", ".h"));
 SetCpp(CPP.replace(".cpp", "Grader.cpp"));
 
 SetCppFlags("-DMYFLAG");
