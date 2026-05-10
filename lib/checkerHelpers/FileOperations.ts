93d19fce2edf007e39053ebc2c9ed341c9639c59
Svetlozar Argirov <zarrro@gmail.com>
2026-05-10 19:55:13 +0700

    Fix Ts Complie errors

--- lib/checkerHelpers/FileOperations.ts
+++ lib/checkerHelpers/FileOperations.ts
@@ -1,9 +1,9 @@
 import { readFileSync } from "fs";
 export function GetFileAs2DArrayOfNumbers(file: string): number[][] {
   const f = readFileSync(file, "utf8");
-  return f.split("\n").map((e) => e.split(" "));
+  return f.split("\n").map((e) => e.split(" ")).map((e) => e.map((e) => parseInt(e)));
 }
-export function GetFileAsArrayOfNumbers(file: string): number[][] {
+export function GetFileAsArrayOfNumbers(file: string): number[] {
   const f = readFileSync(file, "utf8");
-  return f.split("\n").flatMap((e) => e.split(" "));
+  return f.split("\n").flatMap((e) => e.split(" ")).map((e) => parseInt(e));
 }
