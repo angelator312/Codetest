91baf80b1c2d367d3a648f703d7de05d4abfbef6
Svetlozar Argirov <zarrro@gmail.com>
2026-05-10 19:36:11 +0700

    Add github action to make releases

--- build-exe.ts
+++ build-exe.ts
@@ -2,6 +2,9 @@
 
 import { Glob } from "bun";
 
+// process.env.BUN_TARGET controls the target platform for the build
+// valid values are "bun-linux-x64", "bun-linux-arm64", "bun-windows-x64", "bun-windows-arm64", "bun-darwin-arm64"
+
 const stdTestFiles = Array.from(new Glob("stdTest/**/*.js").scanSync("."));
 const typesContent = await Bun.file("./types/index.d.ts").text();
 
@@ -13,10 +16,11 @@ const result = await Bun.build({
     // We need to include types file contant as define, becaseu bun atumatically compiles it to javascript
     // which makes it unavailable at runtime
     "__TYPES_CONTENT__": JSON.stringify(typesContent),
-  },
-  metafile: true,
+  },  
+  metafile: !process.env.BUN_TARGET, // Write metafile only when biulding locally ( no target )
   compile: {
-    outfile: `${outDir}/codetest`,
+    ...(process.env.BUN_TARGET ? { target: process.env.BUN_TARGET as Bun.Build.CompileTarget } : {}),
+    outfile: process.env.BUN_TARGET ? `${outDir}/codetest-${process.env.BUN_TARGET.replace(/bun-/, '')}` : `${outDir}/codetest`,
   },
 });
 
