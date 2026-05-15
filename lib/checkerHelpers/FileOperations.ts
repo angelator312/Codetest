c77845ac6be2d07852acc460d2ddf2ec937dc517
Angel Raev <112373005+angelator312@users.noreply.github.com>
2026-05-15 08:38:49 +0300

    added more FileOperations
    Co-authored-by: Svetlozar Argirov <zarrro@gmail.com>

--- lib/checkerHelpers/FileOperations.ts
+++ lib/checkerHelpers/FileOperations.ts
@@ -41,7 +41,6 @@ export function GetFileAsArrayOfOptionalSequences(
 type TestCaseLines = string[];
 interface SimpleTestCase {
   n: number;
-<<<<<<< HEAD
   line: string;
 }
 export function GetFileAsArrayOfSimpleTestCases(file: string) {
@@ -59,37 +58,3 @@ export function GetFileAsArrayOfSimpleTestCases(file: string) {
   // console.log(out);
   return out;
 }
-//
-/* (
-  (tests[i].line[j] == "1") != binary_search(f2[i], tests[i].n, j + 1),
-);
-number - float
-// export function GetFileAsArrayOfOptionalSequences(){}
-/*
-T
-N
-10101010101010100101
-11010010101010100101
-for i in 1..n s2+=bs(i)
-N
-
-out:
-permutation(N)
-
-function binary_search(p:number[], n:number, target:number) :bool{
-  let left = 0, right = n - 1;
-  while (left < right) {
-    let mid = (left + right) / 2;
-    if (p[mid] == target) return true;
-    else if (p[mid] < target) left = mid + 1;
-    else right = mid - 1;
-  }
-  if (p[left] == target) return true;
-  else return false;
-}
-*/
-=======
-  lines: TestCaseLines;
-}
-// export function GetFileAsArrayOfOptionalSequences(){}
->>>>>>> 1ea04d8 (added copy dev js and some helpers)
