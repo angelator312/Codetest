e9832abd22558c933de483b4d8d6051641dbcaca
Angelator312 <angelator312@angelator312.top>
2026-08-03 13:14:49 +0300

    added a way to get the #define cerr if(0) cerr to stop the There's a cerr warning

--- lib/SearchForCerr.ts
+++ lib/SearchForCerr.ts
@@ -1,8 +1,13 @@
 const one_line_commment = /\/\/.*?$/gms;
 const block_commment = /(?<!\/)\/\*.*?\*\//gms;
 const cerr_find = /\bcerr\b/gms;
+const define_cerr_as_nothing = /\s*#define\s+cerr\s+if\(0\)\s*cerr/gm;
 export function IsThereACerr(code: string) {
   code = code.replaceAll(block_commment, "");
   code = code.replaceAll(one_line_commment, "");
+  let a = define_cerr_as_nothing.test(code);
+  if (a) {
+    return false;
+  }
   return cerr_find.test(code);
 }
