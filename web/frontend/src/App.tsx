1871ad6cbc69d733a7bb0e3849e057d0a2737fef
Angelator312 <angelator312@angelator312.top>
2026-08-04 08:49:50 +0300

    init + pretty done parameters tab

---
+++ web/frontend/src/App.tsx
@@ -0,0 +1,79 @@
+import { useState } from 'react'
+import './App.css'
+import { Button, Flex, Group, Input, MantineProvider, RangeSlider, Splitter, Stack, Switch, Tabs, TextInput } from '@mantine/core'
+import '@mantine/core/styles.css';
+function App() {
+  const [count, setCount] = useState(0)
+  const [params, setParams] = useState<{ variable: string, range: [number, number] }[]>([{ variable: "N", range: [1, 100] }, { variable: "N", range: [1, 100] }]);
+  return (
+    <MantineProvider>
+      <Splitter style={{ height: "100%", width: "100%", flex: 1 }}>
+        <Splitter.Pane defaultSize={50}>
+          <Tabs defaultValue={"params"}>
+            <Tabs.List>
+              <Tabs.Tab value="cpp_files" >
+                Cpp Files
+              </Tabs.Tab>
+              <Tabs.Tab value="js_files" >
+                JS Files
+              </Tabs.Tab>
+              <Tabs.Tab value="params" >
+                Parameters
+              </Tabs.Tab>
+              <Button>
+                Run
+              </Button>
+            </Tabs.List>
+            <Tabs.Panel value='params'>
+              <Stack>
+                <TextInput label="cppFile" />
+                <TextInput label="jsFile" />
+                <Stack>
+                  {params.map((e, i) => (
+                    <div
+                      key={i}
+                    >
+                      <Group grow preventGrowOverflow={false}>
+
+                        <TextInput style={{ flex: "0 0 5ch" }} defaultValue={e.variable} />
+                        :
+                        <RangeSlider
+                          color="blue"
+                          defaultValue={e.range}
+                          max={500}
+                        />
+                      </Group>
+                    </div>
+                  ))}
+                  <Button onClick={() => {
+                    console.log(params, params.concat({ range: [1, 10], variable: "HMM" }))
+                    setParams(params.concat([{ range: [1, 10], variable: "HMM" }]))
+                  }
+                  }>Add params</Button>
+                  <Switch label="verbose output?" name='verbose' />
+                </Stack>
+              </Stack>
+            </Tabs.Panel>
+          </Tabs>
+        </Splitter.Pane>
+        <Splitter.Pane defaultSize={50} bg="teal">
+          <div>
+            <h1>WIP</h1>
+            <p>
+              Nothing done <code>src/App.tsx</code> and save to test <code>HMR</code>
+            </p>
+          </div>
+          <button
+            type="button"
+            className="counter"
+            onClick={() => setCount((count) => count + 1)}
+          >
+            Count is {count}
+          </button>
+        </Splitter.Pane>
+      </Splitter>
+    </MantineProvider>
+  )
+}
+
+export default App
