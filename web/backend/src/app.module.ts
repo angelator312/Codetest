376b389ae69f0f352ce408d23da28369e154736d
Angelator312 <angelator312@angelator312.top>
2026-08-08 14:48:38 +0300

    added if you run the backend to serve the frontend(if frontend is builded)

--- web/backend/src/app.module.ts
+++ web/backend/src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FilesModule } from './files/files.module';
 import { RunModule } from './run/run.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [FilesModule, RunModule],
+  imports: [FilesModule, RunModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', '..','frontend','dist'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
