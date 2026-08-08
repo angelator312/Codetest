4012de4abd81b910ca3b9fada708db18b7e4cf58
Angelator312 <angelator312@angelator312.top>
2026-08-08 08:41:19 +0300

    working fileView

--- web/frontend/src/FileView.tsx
+++ web/frontend/src/FileView.tsx
@@ -1,9 +1,36 @@
-import { Textarea } from "@mantine/core";
-import { useState } from "react";
+import { Button, Textarea } from "@mantine/core";
+import { useEffect, useState } from "react";
 
 export function FileView({ projectId, fileName }: { projectId: string, fileName: string }) {
-    const [file, setFile] = useState(fileName);
+    const [file, setFile] = useState("");
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(`/files/loadFile?id=${projectId}&fileName=${fileName}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'text/plain',
+                },
+            });
+            const data = await response.text();
+            setFile(data)
+        })();
+    }, []);
+    const saveJSON = async () => {
+        const response = await fetch(`/files/saveFile?id=${projectId}&fileName=${fileName}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/plain',
+            },
+            body: file
+        });
+    };
     return (
-        <Textarea value={file} onChange={(event) => setFile(event.currentTarget.value)}/>
+        <Textarea autosize resize="vertical" loading={file == ""} value={file} onChange={(event) => setFile(event.currentTarget.value)}
+            bottomSection={
+                <Button size="compact-sm" onClick={() => { saveJSON() }}>
+                    Save
+                </Button>
+            }
+        />
     );
 }
